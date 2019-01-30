87762078d7f82db30507305726e24d5034509a45
raihanlanka <raihanlankausa@gmail.com>
2019-01-29 23:33:03 -0800

    Add existing file

--- Accounts/views.py
+++ Accounts/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 def index(request):    
-    return render(request, 'index.html')
+    return render(request, 'Accounts/index.html')
